51672e549ae880d36782e243e5869274846a5d2c
csyntax <csyntax@outlook.com>
2016-06-21 21:52:35 +0300

    add FilesAndFolders project

---
+++ FilesAndFolders/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+namespace FilesAndFolders
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var entryPoint = new Folder("Help", @"C:\Windows\Help");
+            
+            GetSubFolders(entryPoint);
+            
+            StringBuilder result = new StringBuilder();
+            
+            GetFileSystemString(entryPoint, result, 0);
+            
+            Console.WriteLine(result);
+        }
+
+        private static void GetFileSystemString(Folder folder, StringBuilder result, int depth)
+        {
+            string indent = new string('.', depth * 3);
+
+            result.AppendLine(string.Format("{0}{1} size: {2}", indent, folder.Name, folder.GetSize()));
+
+            foreach (var file in folder.Files)
+            {
+                result.AppendLine(string.Format(".{0}-{1} size: {2}", indent, file.Name, file.Size));
+            }
+
+            foreach (var subFolder in folder.Folders)
+            {
+                GetFileSystemString(subFolder, result, depth + 1);
+            }
+        }
+        private static void GetSubFolders(Folder folder)
+        {
+            foreach (var file in folder.Directory.GetFiles())
+            {
+                folder.Files.Add(new File(file.Name, file.Length));
+            }
+
+            foreach (var subDir in folder.Directory.GetDirectories())
+            {
+                var subFolder = new Folder(subDir.Name, subDir.FullName);
+            
+                folder.Folders.Add(subFolder);
+            
+                GetSubFolders(subFolder);
+            }
+        }
+    }
+}
